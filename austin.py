679c641c1991fe0d89b71b21cbe65d760b7e9467
Arthurmeade12 <arthurmeade12@gmail.com>
2022-04-08 09:09:20 -0500

    New scripts

---
+++ austin.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+FIRSTMESSAGE = "Austin a m'apprendre comment coder un peu de python!!!"
+"""
+Will you see this comment?
+"""
+# Another comment.
+print(FIRSTMESSAGE)
+print("Un deuxième message ...")
+print("et un troisième ... (:")
+exit()
+print("quelqu'un ne doit pas voir ce message.")
