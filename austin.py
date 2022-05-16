af48aad5578f3ad24653cf7233c446524f9861bd
Arthurmeade12 <arthurmeade12@gmail.com>
2022-05-16 07:17:12 -0500

    Update

--- austin.py
+++ austin.py
@@ -1,11 +1,13 @@
 #!/usr/local/bin/python3
 FIRSTMESSAGE = "Austin a m'apprendre comment coder un peu de python!!!"
-"""
-Will you see this comment?
+MYVAR = """
+opopojpeifwnpigbewo[wUB]
 """
 # Another comment.
 print(FIRSTMESSAGE)
+print(MYVAR)
 print("Un deuxième message ...")
+TACOS = input('Do you like tacos?')
 print("et un troisième ... (:")
 exit()
 print("quelqu'un ne doit pas voir ce message.")
