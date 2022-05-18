8266e155c017ffe7af2776017a0cbd679da93af1
Arthurmeade12 <71673838+Arthurmeade12@users.noreply.github.com>
2022-05-18 17:16:13 -0500

    Update serversync script

--- serversync.sh
+++ serversync.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 cd ~/Minecraft
-java --module-path $JAVAFX --add-modules javafx.controls -jar serversync-4.2.0.jar $@
-exit $?
+if getopts s
+then
+  java --module-path $JAVAFX --add-modules javafx.controls -jar serversync-4.2.0.jar -sp 25598 -r ./quilt $@
+  exit $?
+else
+  case $(hostname) in
+  Arthur-Meade|manjaro)
+    java --module-path $JAVAFX --add-modules javafx.controls -jar serversync-4.2.0.jar -r ~/Library/Application\ Support/PolyMC/instances/Quilt\ Server/.minecraft
+    ;;
+  *)
+    echo 'Where did you get this script?'
+    ;;
+  esac
+fi
