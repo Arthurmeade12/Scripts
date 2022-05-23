2abad0e3ee6395ce2797443f7f9beae7d63f7dec
Arthurmeade12 <71673838+Arthurmeade12@users.noreply.github.com>
2022-05-23 12:47:27 -0500

    Delete old scripts

--- trinity.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tput sgr0
-PEOPLE=(Camille Arthur Laine Baldwin Collins)
-for x in ${PEOPLE[*]}; do
-	echo "Hey $(tput bold && tput setaf 4) $x!$(tput sgr0)"
-	sleep 0.5
-	echo
-done
--- venice.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo $@
-say $@
-FINISHING_MESSAGE="This script has ended!"
-echo $FINISHING_MESSAGE
-exit
