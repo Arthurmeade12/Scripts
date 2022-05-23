bfcbfa8b24311d72ffde01d4ccac8916fefe79a9
Arthurmeade12 <71673838+Arthurmeade12@users.noreply.github.com>
2022-05-23 12:47:45 -0500

    Major revision of start_server2.sh

--- start_server2.sh
+++ start_server2.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
-set -B
+set -euBo pipefail
 ### Settings
-TIMEOUT=60
-UPDATE_PURPUR=true
-BACKUP=true
-UNIVERSE=$MC_DIR/universe
-MC_DIR=~/Minecraft
-PROXY=true
-declare -A CUSTOMJAVAOPTIONS=(
-	[lobby]='-Xms1536M -Xmx3G -Xdock:name=Lobby -DGeyserSkinManager.ForceShowSkins=true -Xdock:icon=./server-icon.png'
-	[survival]='-Xmx3G -Xms2G -Xdock:name="Survival_Server" -DGeyserSkinManager.ForceShowSkins=true -Xdock:icon=./server-icon.png'
-	[ctl]='-Xms2G -Xmx3G -Xdock:name=CTL -DGeyserSkinManager.ForceShowSkins=true -Xdock:icon=./server-icon.png'
-	#[jack]='-Xms2G -Xmx3G -Xdock:name="Jacks_Server" -DGeyserSkinManager.ForceShowSkins=true -Xdock:icon=./server-icon.png'
-	[fabric]='-Xmx5G -Xms4G -Xdock:name=Fabric_Server -Xdock:icon=./server-icon.png'
-	)
-declare -A OPTS=(
-	[lobby]="--server-name Lobby -h 127.0.0.1 -o false --log-pattern lobby.log -W ../universe/lobby -c ../properties/lobby.properties --log-pattern ../logs/lobby/$DATE.log "
-	[survival]="--server-name Survival -h $HOST -o $ONLINE --log-pattern survival.log -W ../universe/survival -c ../properties/survival.properties --log-pattern ../logs/survival/$DATE.log"
-	[ctl]=" --server-name CTL -h $HOST -o $ONLINE --log-pattern ctl.log -W ../universe/ctl -c ../properties/ctl.properties --log-pattern ../logs/ctl/$DATE.log "
-	#[jack]="--server-name Jack's Server -h $HOST -o $ONLINE --log-pattern jack.log -W ../universe/jack -c ../properties/jack.properties --log-pattern ../logs/jack/$DATE.log "
-	[fabric]='--universe ../universe/fabric'
-	)
-### Do not modify past here
+CONFIG=~/Minecraft/config.sh
+#shellcheck source=~/Minecraft/config.sh
+source $CONFIG
 export TIMEOUT
 RED=$(tput setaf 1)
 BLUE=$(tput setaf 20)
@@ -29,10 +11,17 @@ BOLD=$(tput bold)
 WHITE=$(tput setaf 15)
 MAGENTA=$(tput setaf 5)
 RESET=$(tput sgr0)
+#shellcheck disable=SC2034
 YELLOW=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 SERVERS=()
-TIME="$(date +%Y-%d-%m_%H)"
+if [[ $ITERM = true ]]
+then
+	ITERM='-CC'
+else
+	unset ITERM
+fi
+#shellcheck disable=SC2034
 AIKAR='-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true'
 ZGC="-XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:-UseParallelGC -XX:-UseParallelOldGC -XX:-UseG1GC -XX:+UseZGC -Xlog:gc*:logs/ZGC/$TIME.log:time,uptime:filecount=2,filesize=8M"
 out(){
@@ -66,21 +55,24 @@ abort(){
 }
 if [[ $# = 0 ]]
 then
-  read -p "$GREEN$BOLD==>$RESET $WHITE Which server(s) do you want to start? (lobby|survival|ctl|fabric|velocity) " -a SERVERS
+	#shellcheck disable=SC2048 disable=SC2086
+  read -rp "$GREEN$BOLD==>$RESET $WHITE Which server(s) do you want to start? (lobby|survival|ctl|quilt|velocity) " -a SERVERS
 	if [[ $? -eq 142 ]]
 	then
 		abort "No activity for 1 minute."
 	fi
+	#shellcheck disable=SC2048 disable=SC2086
 	set ${SERVERS[*]}
 	SERVERS=()
 fi
 until [[ $# -eq 0 ]]
 do
 	case "$1" in
-		lobby|survival|ctl|fabric)
+		lobby|survival|ctl|quilt)
 			SERVERS+=("$1")
 			;;
 		velocity)
+			SERVERS+=(velocity)
 			VELOCITY=true
 			;;
 		*)
@@ -96,63 +88,82 @@ then
 fi
 ## Backup
 # Ready
-pushd $MC_DIR
-if [[ $BACKUP = true ]]
+pushd "$MC_DIR"
+if [[ $BACKUP = 'true' ]]
 then
 	pushd ./backup
+	#shellcheck disable=SC2048
 	for SERVER in ${SERVERS[*]}
 	do
-		tar -czpkf ./$SERVER/"$TIME".tar.gz ../universe/$SERVER
+		tar -czpkf ./"$SERVER"/"$TIME".tar.gz ../universe/"$SERVER"
     # -p, preserve permissions, -k, do not overwrite an existing file, new one every hour with $TIME
-    rm $(ls -t | tail -1) # always remove oldest copy, aka always have 10 copies
-		popd
+    rm "$(/bin/ls -t1 | tail -1)" # always remove oldest copy, aka always have 10 copies
 	done
 	popd
 fi
-pushd $MC_DIR
-echo 'Updating Purpur ...'
-mv -f ./purpur-1.??.?-????.jar ./.old-jars/old-purpur.jar
-trap 'echo "Interrupt" && mv ./.old-jars/old-purpur.jar ./purpur.jar' 1 2 3 6
-curl -JLO# https://api.pl3x.net/v2/purpur/1.18.2/latest/download
-CURL=$?
-trap -
-case "$CURL" in
-0)
-	echo 'Done.'
-	;;
-*)
-	echo 'Failed, please manually download jar:'
-	cp $OLD_JAR ./old-purpur.jar
-	open https://purpurmc.org/downloads
-	exit 1
-	;;
-esac
-popd
-## Starting of server
-##########################
-##### UNTESTED ALPHA #####
-##########################
-set +x
-trap 'tmux detach && echo "Interrupt" && exit 1' 1 2 3 6
-TYPE='vertical'
-if $(tmux has -t SERVERS)
+if [[ $UPDATE_PURPUR = 'true' ]]
 then
-	for Z in "${SERVERS[*]}"
-	do
-		if 
+	echo 'Updating Purpur ...'
+	mv -f ./purpur-1.??.?-????.jar ./.old-jars/old-purpur.jar
+	trap 'echo "Interrupt" && mv ./.old-jars/old-purpur.jar ./purpur.jar' 1 2 3 6
+	curl -JLO# https://api.pl3x.net/v2/purpur/1.18.2/latest/download
+	CURL=$?
+	trap -
+	case "$CURL" in
+	0)
+		echo 'Done.'
+		;;
+	*)
+		echo 'Failed, please manually download jar from https://purpurmc.org/downloads'
+		cp "$OLD_JAR" ./old-purpur.jar
+		exit 1
+		;;
+	esac
+fi
+## Starting of server
+set -x
+trap 'tmux detach && echo "Interrupt" && exit 1' 1 2 3 6
+if tmux has -t SERVERS 2>/dev/null
+then
   echo 'Session already exists! Attaching ..'
 	tmux attach -t SERVERS
 	exit $?
 else
   tmux new-session -ds SERVERS
 fi
-tmux rename-window -t 0 SERVERS:"${SERVERS[0]}"
+#shellcheck disable=SC2048
 for Z in ${SERVERS[*]}
 do
-	if [[ "$Z" = "${SERVERS[*]}" ]]; then
-		:
-	else
-		tmux neww -t SERVERS -n "$Z"
-	fi
-	tmux send -t SERVERS:"$Z" 'java --version && echo && read -n 4 STOP && exitC-m'
+	case "$Z" in
+	velocity)
+		pushd ./velocity
+		reset(){
+			echo 'false' > ./.velocity_stat
+		}
+		if grep 'true' < ./.velocity_stat 1>/dev/null
+		then
+			echo "$(tput setaf 1)Error: Velocity is already started."
+			continue
+		fi
+		echo 'true' > ./.velocity_stat
+		tmux neww -t SERVERS -c "$MC_DIR/velocity" -n "${Z^}" "caffeinate -ims sudo nice -n -5 /usr/libexec/java_home -v $JAVA_VERSION --exec java $ZGC ${CUSTOMJAVAOPTIONS[velocity]} -jar ../velocity-*.jar ${OPTS[velocity]}"
+		echo 'false' > ./.velocity_stat
+		popd
+		;;
+	quilt)
+		if [[ $AUTOSERVERSYNC = 'true' ]]
+		then
+			echo 'Starting ServerSync ...'
+			tmux neww -t "$SESSION" -n ServerSync -c "$MC_DIR"/quilt "java --module-path $JAVAFX --add-modules javafx.controls -jar serversync-4.2.0.jar -sp $SERVERSYNCPORT"
+		fi
+		tmux neww -t "$SESSION" -c "$MC_DIR"/"$Z" -n "${Z^}" "caffeinate -ims sudo nice -n -5 '/usr/libexec/java_home -v $JAVA_VERSION --exec java $ZGC ${CUSTOMJAVAOPTIONS[quilt]} -jar ../quilt-server-launch.jar --nogui ${OPTS[quilt]}'"
+		;;
+	survival|ctl|lobby)
+		tmux neww -t "$SESSION" -c "$MC_DIR"/"$Z" -n "${Z^}" "caffeinate -ims sudo nice -n -5 '/usr/libexec/java_home -v $JAVA_VERSION --exec java $ZGC ${CUSTOMJAVAOPTIONS[$Z]} -jar ../purpur-*.jar --nogui ${OPTS[$Z]}'"
+		;;
+	esac
 done
+popd
+tmux killw -t SERVERS:0
+tmux $ITERM attach -t SERVERS
+exit 0
