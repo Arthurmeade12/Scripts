added7aeff041f073aa7abe128378f46f708d6ee
Arthurmeade12 <arthurmeade12@gmail.com>
2023-03-22 17:05:25 -0500

    Clean up average.sh

--- average.sh
+++ average.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 usage(){
-  echo "usage: $0 integer integer integer ..."
+  echo "usage: ${0} integer integer integer ..."
 }
-case "$#" in
-0|1)
+case "${#}" in
+'0'|'1')
   usage
   exit 2
   ;;
-*)
+"*")
   :
   ;;
 esac
-SUM=0
-for x in $@
+SUM='0'
+for x in ${@}
 do
   SUM="$(expr "$SUM" + "$x")"
 done
