b4b6e58bd02122e7ec9ad276ce7b08041dc6df4f
Arthurmeade12 <arthurmeade12@gmail.com>
2023-03-22 17:08:29 -0500

    Cleanup backup.sh

--- backup.sh
+++ backup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 MC_DIR=~/Minecraft
-UNIVERSE=$MC_DIR/universe
-BACKUP=$MC_DIR/backup
-cd $UNIVERSE
-ARGS=-dbrST
-for SERVER in $@
+UNIVERSE="${MC_DIR}"/universe
+BACKUP="${MC_DIR}"/backup
+cd "${UNIVERSE}"
+ARGS='-dbrST'
+for SERVER in "${@}"
 do
-  cd $SERVER
-  if [[ -f "$OLD_ZIP" ]]
+  cd "${SERVER}"
+  if [[ -f "${OLD_ZIP}" ]]
   then
     ARGS+=
   fi
-  zip $ARGS $(date "+%Y-%d-%m %H:%M")
+  zip "${ARGS}" "$(date "+%Y-%d-%m %H:%M")"
 done
